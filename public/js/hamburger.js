5a6b829603f07d89e76bc2af2d251b12c06078d7
H-ymt <y.handai1272@gmail.com>
2023-09-06 15:12:40 +0900

    update:アクセシビリティに考慮したハンバーガーメニューにする

--- public/js/hamburger.js
+++ public/js/hamburger.js
@@ -3,17 +3,47 @@ const bars = document.querySelector(".js-bars");
 const xmark = document.querySelector(".js-xmark");
 const menu = document.querySelector(".js-menu");
 const overlay = document.querySelector(".js-overlay");
+let flag = false;
 
 button.addEventListener("click", (event) => {
   bars.classList.toggle("hidden");
   xmark.classList.toggle("hidden");
   menu.classList.toggle("translate-x-full");
   overlay.classList.toggle("hidden");
+
+  // aria-expandedの制御
+  if (flag) {
+    button.setAttribute("aria-expanded", "false");
+    flag = false;
+  } else {
+    button.setAttribute("aria-expanded", "true");
+    flag = true;
+  }
 });
 
+// 背景のオーバーレイをクリックしてメニューを閉じられるように
 overlay.addEventListener("click", (event) => {
   bars.classList.toggle("hidden");
   xmark.classList.toggle("hidden");
   menu.classList.toggle("translate-x-full");
   overlay.classList.toggle("hidden");
+  button.setAttribute("aria-expanded", "false");
+  flag = false;
+});
+
+// escキーでメニューを閉じられるように
+window.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    bars.classList.toggle("hidden");
+    xmark.classList.toggle("hidden");
+    menu.classList.toggle("translate-x-full");
+    overlay.classList.toggle("hidden");
+    button.setAttribute("aria-expanded", "false");
+    flag = false;
+  }
+});
+
+// フォーカストラップ制御
+focusTrap.addEventListener("focus", (e) => {
+  button.focus();
 });

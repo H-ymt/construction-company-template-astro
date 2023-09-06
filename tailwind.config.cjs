81740e214269c10242aabdd0dfece98f7fcfc111
H-ymt <y.handai1272@gmail.com>
2023-09-06 17:19:21 +0900

    update:Tailwind CSSでフォントを読み込む

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   future: { hoverOnlyWhenSupported: true },
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    fontFamily: {
+      notoSans: ["Noto Sans JP", "sans-serif"],
+    },
     extend: {
       green: {
         50: "#effaf5",

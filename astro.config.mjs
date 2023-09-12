aebe28c6b7808323398e3dd0dd91bbbd4e028da6
H-ymt <y.handai1272@gmail.com>
2023-09-12 14:42:12 +0900

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: vercel({
-    functionPerRoute: false,
-  }),
+  adapter: vercel(),
 });

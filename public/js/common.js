92a0bc9416b81b11ac40434ee220c479ecb77f20
H-ymt <y.handai1272@gmail.com>
2023-09-12 10:53:08 +0900

    add:施工実績を追加する

--- public/js/common.js
+++ public/js/common.js
@@ -3,7 +3,10 @@ const setViewport = () => {
   const viewport = document.querySelector('meta[name="viewport"]');
 
   const switchViewport = () => {
-    const value = window.outerWidth > 360 ? "width=device-width,initial-scale=1" : "width=360";
+    const value =
+      window.outerWidth > 360
+        ? "width=device-width,initial-scale=1"
+        : "width=360";
     if (viewport.getAttribute("content") !== value) {
       viewport.setAttribute("content", value);
     }

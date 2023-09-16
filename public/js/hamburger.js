b8cbbcd58e0e5304260d7a357822a409cb6f66d0
H-ymt <y.handai1272@gmail.com>
2023-09-16 11:38:41 +0900

    update: jsファイルをimportで読み込む

--- public/js/hamburger.js
+++ public/js/hamburger.js
@@ -4,43 +4,27 @@ const hamburgerToggle = () => {
   const xmark = document.querySelector(".js-xmark");
   const menu = document.querySelector(".js-menu");
   const overlay = document.querySelector(".js-overlay");
-  let flag = false;
+
+  const toggleMenu = (expanded) => {
+    bars.classList.toggle("hidden");
+    xmark.classList.toggle("hidden");
+    menu.classList.toggle("translate-x-full");
+    overlay.classList.toggle("hidden");
+    button.setAttribute("aria-expanded", expanded);
+  };
 
   button.addEventListener("click", (event) => {
-    bars.classList.toggle("hidden");
-    xmark.classList.toggle("hidden");
-    menu.classList.toggle("translate-x-full");
-    overlay.classList.toggle("hidden");
-
-    // aria-expandedの制御
-    if (flag) {
-      button.setAttribute("aria-expanded", "false");
-      flag = false;
-    } else {
-      button.setAttribute("aria-expanded", "true");
-      flag = true;
-    }
+    const expanded = button.getAttribute("aria-expanded") === "true";
+    toggleMenu(expanded ? "false" : "true");
   });
 
-  // 背景のオーバーレイをクリックしてメニューを閉じられるように
   overlay.addEventListener("click", (event) => {
-    bars.classList.toggle("hidden");
-    xmark.classList.toggle("hidden");
-    menu.classList.toggle("translate-x-full");
-    overlay.classList.toggle("hidden");
-    button.setAttribute("aria-expanded", "false");
-    flag = false;
+    toggleMenu("false");
   });
 
-  // escキーでメニューを閉じられるように
   window.addEventListener("keydown", (event) => {
     if (event.key === "Escape") {
-      bars.classList.toggle("hidden");
-      xmark.classList.toggle("hidden");
-      menu.classList.toggle("translate-x-full");
-      overlay.classList.toggle("hidden");
-      button.setAttribute("aria-expanded", "false");
-      flag = false;
+      toggleMenu("false");
     }
   });
 };

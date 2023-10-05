01b711485c6c11ebb32b0074d15151fc9f13fe02
H-ymt <y.handai1272@gmail.com>
2023-10-05 18:08:59 +0900

    update: 対象のクラスの要素のみを対象に処理が実行されるようにする

--- public/js/works.js
+++ public/js/works.js
@@ -1,12 +1,16 @@
-const loadMore = () => {
-  const loadMoreButton = document.getElementById("js-load-more");
+const loadMore = (itemsToShow = 5, targetClass = ".item") => {
+  const loadMoreButton = document.querySelector(".js-load-more");
   if (loadMoreButton) {
     loadMoreButton.addEventListener("click", () => {
-      const hiddenItems = document.querySelectorAll(".hidden");
-      hiddenItems.forEach((item) => {
+      const hiddenItems = Array.from(
+        document.querySelectorAll(`${targetClass}.hidden`),
+      );
+      hiddenItems.slice(0, itemsToShow).forEach((item) => {
         item.classList.remove("hidden");
       });
-      loadMoreButton.style.display = "none";
+      if (hiddenItems.length <= itemsToShow) {
+        loadMoreButton.style.display = "none";
+      }
     });
   }
 };
